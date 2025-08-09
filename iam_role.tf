68e7f1e93170e47e5250a94f424ad10e13677f8d
Dinesh Mohan <dinesh22raj98@gmail.com>
2025-08-09 16:40:05 +0800

    Fourth commit

--- iam_role.tf
+++ iam_role.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "ec2_role" {
     Version = "2012-10-17",
     Statement = [{
       Action = "sts:AssumeRole",
-      Principal = {
+      Principal = {  
         Service = "ec2.amazonaws.com"
       },
       Effect = "Allow",
