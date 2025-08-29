15938c4757012143ce74e4d0497ba146c4990024
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-08-29 23:08:46 +0800

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "ec2_role" {
-  name = "ec2_s3_access"
+  name = "ec2_codedeploy_role"
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -15,11 +15,6 @@ resource "aws_iam_role" "ec2_role" {
 resource "aws_iam_role_policy_attachment" "ec2_codedeploy_policy_attach" { 
   role = aws_iam_role.ec2_codedeploy_role.name 
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy" 
-}
-
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2-instance-profile"
-  role = aws_iam_role.ec2_role.name
 }
 
 resource "aws_iam_instance_profile" "ec2_codedeploy_instance_profile" { 
