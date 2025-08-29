6821b8f311ebdd605c18d86dbb1870a6511d9d0d
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-08-30 00:49:44 +0800

    Update iam_role.tf

--- iam_role.tf
+++ iam_role.tf
@@ -22,14 +22,14 @@ resource "aws_iam_role_policy_attachment" "ec2_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
 }
 
-resource "aws_iam_role_policy_attachment" "ec2_codedeploy_attach" { 
-  role = aws_iam_role.ec2_codedeploy_role.name 
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforAWSCodeDeploy"
-}
-
 resource "aws_iam_role_policy_attachment" "s3_full_access" { 
   role = aws_iam_role.ec2_codedeploy_role.name 
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "codedeploy_service_role_attach" { 
+  role = aws_iam_role.ec2_codedeploy_role.name 
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSCodeDeployRole"
 }
 
 resource "aws_iam_instance_profile" "ec2_codedeploy_instance_profile" { 
