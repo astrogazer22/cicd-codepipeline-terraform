ee6e878ee24e5d0ce48f6aaebfcbfd8064853e4f
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-08-30 17:33:17 +0800

    Create elastic_ip.tf

---
+++ elastic_ip.tf
@@ -0,0 +1,13 @@
+# Create Elastic IP
+resource "aws_eip" "web_eip" {
+  vpc = true
+  tags = {
+    Name = "nodejs-web-eip"
+  }
+}
+
+# Associate Elastic IP with your EC2 instance
+resource "aws_eip_association" "web_eip_assoc" {
+  instance_id   = aws_instance.nodejs_app.id
+  allocation_id = aws_eip.web_eip.id
+}
