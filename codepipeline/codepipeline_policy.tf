c1fec60f70b2afea66e6aed5f04b4e8b2275f3ab
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-01 13:13:07 +0800

    Update codepipeline_policy.tf

--- codepipeline/codepipeline_policy.tf
+++ codepipeline/codepipeline_policy.tf
@@ -45,3 +45,8 @@ resource "aws_iam_policy" "codepipeline_policy" {
     ]
   })
 }
+
+resource "aws_iam_role_policy_attachment" "codepipeline_policy_attach" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = aws_iam_policy.codepipeline_policy.arn
+}
