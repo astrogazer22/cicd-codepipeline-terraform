f350f4ef007d26dcc4dac4b82c69b1302f7c681c
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-01 13:13:36 +0800

    Update codedeploy_policy.tf

--- codedeploy/codedeploy_policy.tf
+++ codedeploy/codedeploy_policy.tf
@@ -76,6 +76,11 @@ resource "aws_iam_policy" "codedeploy_policy" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "AWSCodeDeployRole" {
+  policy_arn = aws_iam_policy.codedeploy_policy.arn
+  role       = aws_iam_role.codedeploy_role.name
+}
+
 # Attach policy to the role
 resource "aws_iam_role_policy_attachment" "codedeploy_policy_attach" {
   role       = aws_iam_role.codedeploy_service_role.name
