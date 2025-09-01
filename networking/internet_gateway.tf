57376e4d5164f43c6777220c7c6c594046cc276e
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-01 17:13:48 +0800

    Update internet_gateway.tf

--- networking/internet_gateway.tf
+++ networking/internet_gateway.tf
@@ -10,7 +10,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.cidr_block_all
     gateway_id = aws_internet_gateway.gw.id
   }
 }
