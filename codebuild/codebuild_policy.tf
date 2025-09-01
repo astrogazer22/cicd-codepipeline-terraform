0508bd10989d9c95060d2dad8854b380078c7fd3
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-01 13:10:08 +0800

    Update and rename codebuild_attach.tf to codebuild_policy.tf

--- codebuild/codebuild_policy.tf
+++ codebuild/codebuild_policy.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role_policy_attachment" "codebuild_attach" {
+resource "aws_iam_role_policy_attachment" "codebuild_policy" {
   role       = aws_iam_role.codebuild_service_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
 }
