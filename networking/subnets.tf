e1360176ad25e4d1701511972bef57e62bf52509
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-01 17:15:23 +0800

    Update subnets.tf

--- networking/subnets.tf
+++ networking/subnets.tf
@@ -1,9 +1,9 @@
 resource "aws_subnet" "public" {
   vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "ap-southeast-1a"
+  cidr_block        = var.cidr_block_vpc
+  availability_zone = var.availability_zone
 
   tags = {
-    Name = "public subnet"
+    Name = var.tag_subnet
   }
 }
