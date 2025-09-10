890ad020360ba373eb8c4797ab79029ef57ba1f0
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-10 16:43:40 +0800

    Create variables.tf

---
+++ codepipeline/variables.tf
@@ -0,0 +1,111 @@
+variable "codepipeline_source_name" {
+  type = string
+}
+
+variable "codepipeline_stage_source" {
+  type = string
+}
+
+variable "codepipeline_category_source" {
+  type = string
+}
+
+variable "codepipeline_source_owner" {
+  type = string
+}
+
+variable "codepipeline_source_provider" {
+  type = string
+}
+
+variable "codepipeline_version" {
+  type = number
+}
+
+variable "codepipeline_source_output_artifacts" {
+  type = string
+}
+
+variable "codepipeline_repository" {
+  type = string
+}
+
+variable "codepipeline_branch_name" {
+  type = string
+}
+
+variable "codepipeline_build_name" {
+  type = string
+}
+
+variable "codepipeline_build_stage" {
+  type = string
+}
+
+variable "codepipeline_category_build" {
+  type = string
+}
+
+variable "codepipeline_build_owner" {
+  type = string
+}
+
+variable "codepipeline_build_provider" {
+  type = string
+}
+
+variable "codepipeline_build_output_artifacts" {
+  type = string
+}
+
+variable "codepipeline_build_input_artifacts" {
+  type = string
+}
+
+variable "codepipeline_deploy_name" {
+  type = string
+}
+
+variable "codepipeline_deploy_stage" {
+  type = string
+}
+
+variable "codepipeline_category_deploy" {
+  type = string
+}
+
+variable "codepipeline_deploy_owner" {
+  type = string
+}
+
+variable "codepipeline_deploy_provider" {
+  type = string
+}
+
+variable "codepipeline_deploy_input_artifacts" {
+  type = string
+}
+
+variable "aws_connection_name" {
+  type = string
+}
+
+variable "aws_connection_provider_type" {
+  type = string
+}
+
+variable "codepipeline_project_name" {
+  type = string
+}
+
+variable "codepipeline_application_name" {
+  type = string
+}
+
+variable "codepipeline_deployment_group" {
+  type = string
+}
+
+variable "codepipeline_artifact_location" {
+  type = string
+}
