0e1e00089142bb9b03dc08c5d2c5afe994015a3c
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-10 17:54:34 +0800

    Delete s3_bucket directory

--- s3_bucket/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "astrogazer_nodejs_s3_bucket" {
-  bucket = "astrogazer-nodejs-s3-bucket"
-
-  tags = {
-    Name        = var.tag_s3_name
-    Environment = var.tag_s3_environment
-  }
-}
--- s3_bucket/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "tag_s3_name" {
-  type = string
-}
-
-variable "tag_s3_environment" {
-  type = string
-}
