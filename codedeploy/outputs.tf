7b3bcfd932e51e85763613138c98d25710a456f1
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-10 17:53:22 +0800

    Delete codedeploy directory

--- codedeploy/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "codedeploy_nodejs_app_name" {
-  value = aws_codedeploy_app.nodejs_app_deploy.name
-}
-
-output "codedeploy_deployment_group_name" {
-  value = aws_codedeploy_deployment_group.my_deploy_group.app_name
-}
