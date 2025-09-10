44b48b64d2ce9765d97c1cf91e87768493d6120d
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-10 17:25:20 +0800

    Update variables.tf

--- modules/cloudwatch/variables.tf
+++ modules/cloudwatch/variables.tf
@@ -1,1 +1,43 @@
+variable "alarm_name" {
+  type = string
+}
 
+variable "comparison_operator" {
+  type = string
+}
+
+variable "metric_name" {
+  type = string
+}
+
+variable "cloudwatch_evaluation_period" {
+  type = number
+}
+
+variable "namespace" {
+  type = string
+}
+
+variable "metric_period" {
+  type = number
+}
+
+variable "metric_statistic" {
+  type = string
+}
+
+variable "metric_threshold" {
+  type = number
+}
+
+variable "log_retention_period" {
+  type = number
+}
+
+variable "cloudwatch_log_path" {
+  type = string
+}
+
+variable "cloudwatch_instance_id" {}
+
+variable "cloudwatch_alarm_actions" {}
