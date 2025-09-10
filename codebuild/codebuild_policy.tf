bb923c196f58820e3b78df326407d0c6b005553c
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-10 16:38:59 +0800

    Delete codebuild/codebuild_policy.tf

--- codebuild/codebuild_policy.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_iam_role_policy_attachment" "codebuild_policy" {
-  role       = aws_iam_role.codebuild_service_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
-}
