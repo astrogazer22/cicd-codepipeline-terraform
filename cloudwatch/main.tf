59d74932e536401541e099cc5187ed8d8d385e68
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-10 16:45:26 +0800

    Create main.tf

---
+++ cloudwatch/main.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudwatch_log_group" "ec2_logs" {
+  name              = var.cloudwatch_log_path
+  retention_in_days = var.log_retention_period
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = var.alarm_name
+  comparison_operator = var.comparison_operator
+  evaluation_periods  = var.cloudwatch_evaluation_period
+  metric_name         = var.metric_name
+  namespace           = var.namespace
+  period              = var.metric_period
+  statistic           = var.metric_statistic
+  threshold           = var.metric_threshold
+  alarm_description   = "This alarm triggers if CPU usage exceeds ${var.metric_threshold} for ${var.metric_period} minutes"
+
+  dimensions = {
+    InstanceId = var.cloudwatch_instance_id
+  }
+}
+
+
+
+
+
+
