b346e8efc84fe6e61ec517f5f3e2d4a008b395c2
Dinesh Mohan <154987592+astrogazer22@users.noreply.github.com>
2025-09-10 16:57:13 +0800

    Delete networking/subnets.tf

--- networking/subnets.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = var.cidr_block_vpc
-  availability_zone = var.availability_zone
-
-  tags = {
-    Name = var.tag_subnet
-  }
-}
